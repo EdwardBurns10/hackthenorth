07e3ca7b97b2f343c8ea304b7b24026c0d74136d
Lia Mason <lia.k.mason@gmail.com>
2021-01-16 14:12:21 -0500

    start layout

--- goal-tracker/pages/index.js
+++ goal-tracker/pages/index.js
@@ -1,65 +1,115 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import styled from "styled-components"
+import { Container, Grid, Card, CardContent, Typography } from '@material-ui/core'
 
-export default function Home() {
+const Content = styled.div`
+  padding: 0 3% 0 3%;
+`
+const Name = styled.h2`
+  font-size: 20px;
+  position: absolute;
+  left: 0;
+`
+const Subtitle = styled.p`
+  font-size: 10px;
+  position: absolute;
+  left: 0;
+  padding-top: 35px;
+`
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding-bottom: 10px;
+`
+const Headerline = styled.hr`
+  width: 100%;
+  margin-bottom: 20px;
+  border: 1px solid #F2F2F2;
+`
+const Widget = styled(Card)`
+  width: 20%;
+  justify-content: center;
+`
+const cardTitle = styled(Typography)`
+  font-size: 20px;
+  font-weight: 800;
+  text-align: center;
+`
+
+const Home = () => {
   return (
-    <div className={styles.container}>
+    <Content>
       <Head>
-        <title>Create Next App</title>
+        <title>lifemapp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <Header> 
+        <span>
+          <Name>hillary leung</Name>
+          <Subtitle>week's productivity: 85%</Subtitle>
+        </span>
+        <h1>lifemapp</h1>
+      </Header>
+      <Headerline></Headerline>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>personal mission</cardTitle>
+               <p>brief powerful statement that inspires and guides you on your path through life.</p>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>2021 goals</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>Q1 (jan-mar)</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>january</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>5 year goals</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={3}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>today's tasks</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card style={{ backgroundColor: "#F2F2F2" }}>
+            <CardContent>
+               <cardTitle>week's schedule</cardTitle>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Content>
   )
 }
+
+export default Home
