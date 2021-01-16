0bed8d9cc777009b575f633513d7fa0e08be1a81
Lia Mason <lia.k.mason@gmail.com>
2021-01-16 15:40:47 -0500

    add demo goals

--- goal-tracker/pages/index.js
+++ goal-tracker/pages/index.js
@@ -7,7 +7,7 @@ import { StylesProvider } from '@material-ui/core/styles';
 const Content = styled.div`
   && {
     padding: 0 3% 0 3%;
-    height: 100vh;
+    //height: 100vh;
   }
 `
 const Name = styled.h2`
@@ -38,20 +38,71 @@ const Headerline = styled.hr`
     border: 1px solid #F2F2F2;
   }
 `
-const Widget = styled(Card)`
+const Widget1 = styled(Card)`
   && {
     width: 100%;
-    height: 40vh;
+    height: 30vh;
     text-align: center;
     margin-top: 3px;
     margin-bottom: 3px;
+    padding: 0 10px 0 10px;
+    h3 {
+      font-size: 18px;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    h4 {
+      margin-top: 0px;
+      font-size: 11px;
+      color: #8A8A8A;
+    }
+    p {
+      font-size: 18px;
+      color: #8A8A8A;
+    }
   }
 `
-const cardTitle = styled.p`
+const Widget2 = styled(Card)`
   && {
-    font-size: 20px;
-    font-weight: 800;
+    width: 100%;
+    min-height: 50vh;
     text-align: center;
+    margin-top: 3px;
+    margin-bottom: 3px;
+    padding: 0 10px 0 10px;
+    h3 {
+      font-size: 18px;
+      font-weight: bold;
+      margin-bottom: 5px;
+    }
+    h4 {
+      margin-top: 0px;
+      font-size: 11px;
+      color: #8A8A8A;
+    }
+    p {
+      font-size: 18px;
+      color: #8A8A8A;
+    }
+  }
+`
+const FiveYearGoal = styled.div`
+  && {
+    text-align: left;
+    background-color: white;
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+    width: 100%;
+    ul {
+      list-style-type: disc;
+      padding-left: 25px;
+    }
+    li {
+      color: black;
+      font-size: 11px;
+      margin: 5px 0 5px 0;
+    }
   }
 `
 const Home = () => {
@@ -73,56 +124,85 @@ const Home = () => {
         <div>
           <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2"}}>
+              <Widget1 style={{ backgroundColor: "#F2F2F2"}}>
                 <CardContent>
-                  <cardTitle>personal mission</cardTitle>
+                  <h3>personal mission</h3>
                   <p>brief powerful statement that inspires and guides you on your path through life.</p>
                 </CardContent>
-              </Widget>
+              </Widget1>
             </Grid>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget1 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>2021 goals</cardTitle>
+                  <h3>2021 goals</h3>
+                  <h4>YEARLY GOALS</h4>
                 </CardContent>
-              </Widget>
+              </Widget1>
             </Grid>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget1 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>Q1 (jan-mar)</cardTitle>
+                  <h3>Q1 (jan-mar)</h3>
+                  <h4>QUARTERLY GOALS</h4>
                 </CardContent>
-              </Widget>
+              </Widget1>
             </Grid>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget1 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>january</cardTitle>
+                  <h3>january</h3>
+                  <h4>MONTHLY GOALS</h4>
                 </CardContent>
-              </Widget>
+              </Widget1>
             </Grid>
           </Grid>
-          <Grid container direction="row" justify="center" alignItems="center" spacing={2}>
+          <Grid container direction="row" justify="center" alignItems="top" spacing={2}>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget2 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>5 year goals</cardTitle>
+                  <h3>5 year goals</h3>
+                  <div style={{paddingTop:"10px"}}>
+                    <FiveYearGoal>
+                      <h4>MOST IMPORTANT</h4>
+                      <ul>
+                        <li>a very important goal</li>
+                        <li>another very important goal</li>
+                        <li>something you really want</li>
+                      </ul>
+                    </FiveYearGoal>
+                    <FiveYearGoal>
+                      <h4>IMPORTANT</h4>
+                      <ul>
+                        <li>a important goal</li>
+                        <li>another important goal</li>
+                        <li>something you want</li>
+                      </ul>
+                    </FiveYearGoal>
+                    <FiveYearGoal>
+                      <h4>LESS IMPORTANT</h4>
+                      <ul>
+                        <li>something nice to achieve</li>
+                        <li>another nice-to-have</li>
+                        <li>something you kinda want</li>
+                      </ul>
+                    </FiveYearGoal>
+                  </div>
                 </CardContent>
-              </Widget>
+              </Widget2>
             </Grid>
             <Grid item xs={3}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget2 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>today's tasks</cardTitle>
+                  <h3>today's tasks</h3>
                 </CardContent>
-              </Widget>
+              </Widget2>
             </Grid>
             <Grid item xs={6}>
-              <Widget style={{ backgroundColor: "#F2F2F2" }}>
+              <Widget2 style={{ backgroundColor: "#F2F2F2" }}>
                 <CardContent>
-                  <cardTitle>week's schedule</cardTitle>
+                  <h3>week's schedule</h3>
                 </CardContent>
-              </Widget>
+              </Widget2>
             </Grid>
           </Grid>
         </div>
