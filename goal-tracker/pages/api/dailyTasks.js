fbb3eb2e395f8645555e263c324c4e34aa845b3f
Edward Burns <eburns@redcanari.com>
2021-01-16 20:46:49 -0500

    .findOne()

--- goal-tracker/pages/api/dailyTasks.js
+++ goal-tracker/pages/api/dailyTasks.js
@@ -7,9 +7,9 @@ const handler = nextConnect();
 handler.use(middleware);
 
 handler.get(async (req, res) => {
-  let doc = await req.db.collection('dailyTasks')
-  //console.log(doc);
-  res.json(doc);
+    let doc = await req.db.collection('dailyTasks').findOne()
+    console.log(doc);
+    res.json(doc);
 });
 
 
--- goal-tracker/pages/index.js
+++ goal-tracker/pages/index.js
@@ -10,16 +10,15 @@ const Home = ({data}) => {
 
 console.log('hello')
 
+    // console.log('here')
+    // const res =  fetch('http://localhost:3000/api/dailyTasks')
+    // console.log(res)
+
   return (
       <div className={styles.container}>
           {console.log(results)}
       </div>
   )
-    Home.getInitialProps = async () => {
-        const res = await fetch('http://localhost:3000/api/dailyTasks')
-        const json = await res.json()
-        return { data: json }
-    }
-    console.log(json)
+
 }
 export default Home
